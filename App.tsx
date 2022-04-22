2398854953699c760ec5e133151538e2e0fe0eb8
mostafaelspagh0 <mostafaelspagh0@gmail.com>
2022-04-23 01:49:51 +0200

    app Starts with Drawer Stack

--- App.tsx
+++ App.tsx
@@ -2,17 +2,18 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import MainStack from './src/navigation/MainStack'
 import { Provider } from 'react-redux'
 import { store } from './src/redux/store'
+import { StyleSheet } from 'react-native'
+import DrawerStack from './src/navigation/DrawerStack'
 
 const App = () => {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={StyleSheet.absoluteFill}>
           <NavigationContainer>
-            <MainStack />
+            <DrawerStack />
           </NavigationContainer>
         </GestureHandlerRootView>
       </SafeAreaProvider>
