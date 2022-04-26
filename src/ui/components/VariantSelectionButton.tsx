9b0c55caa22e2537348e48c113f5abe8bb841ea1
mostafaelspagh0 <mostafaelspagh0@gmail.com>
2022-04-26 05:00:23 +0200

    fix linting error

--- src/ui/components/VariantSelectionButton.tsx
+++ src/ui/components/VariantSelectionButton.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import React from 'react'
 import Ripple from './Ripple'
 type buttonType = {
