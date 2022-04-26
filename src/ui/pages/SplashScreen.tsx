9ed0b143efd6acd4e9896fff22312e85923b4858
mostafaelspagh0 <mostafaelspagh0@gmail.com>
2022-04-26 17:44:03 +0200

    fix linting error

--- src/ui/pages/SplashScreen.tsx
+++ src/ui/pages/SplashScreen.tsx
@@ -22,7 +22,6 @@ const SplashScreen = () => {
       <Image
         source={require('../images/splash.png')}
         resizeMethod="resize"
-        // eslint-disable-next-line react-native/no-inline-styles
         style={{
           alignSelf: 'center',
           height: ScreenHighet * 0.5,
@@ -33,7 +32,7 @@ const SplashScreen = () => {
       <View
         style={{
           marginHorizontal: 20,
-          marginBottom: "8%",
+          marginBottom: '8%',
         }}
       >
         <LargeBottomButton
