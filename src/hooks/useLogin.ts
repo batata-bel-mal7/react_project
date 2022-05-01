57e5b11ca6b9966c81cce19e4227026c481cabf0
mostafaelspagh0 <mostafaelspagh0@gmail.com>
2022-05-01 05:20:14 +0200

    create use login hook

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { selectUser } from '../redux/user/userSlice'
+import {
+  loginWithEmailAndPassword as loginWithEmailAndPasswordEvent,
+  logout as logoutEvent,
+} from '../redux/user/userSlice'
+
+const useLogin = () => {
+  const user = useSelector(selectUser)
+  const appDispatch = useDispatch()
+
+  const loginWithEmailAndPassword = (email: string, password: string) => {
+    appDispatch(loginWithEmailAndPasswordEvent({ email, password }) as any)
+  }
+
+  const logout = () => {
+    appDispatch(logoutEvent() as any)
+  }
+
+  return { loginWithEmailAndPassword, logout, user }
+}
+
+export default useLogin
