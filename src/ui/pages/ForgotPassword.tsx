24eb012d259b4cf86efdf591f87ed36cc133f5f3
mostafaelspagh0 <mostafaelspagh0@gmail.com>
2022-05-01 02:44:51 +0200

    code format

--- src/ui/pages/ForgotPassword.tsx
+++ src/ui/pages/ForgotPassword.tsx
@@ -1,39 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react'
-import { TouchableOpacity,Image,TextInput,StyleSheet, Text, View, Button } from 'react-native'
-import Header from '../components/Header';
-import BackIcon from '../components/icons/BackIcon';
-import HeaderTitle from '../components/HeaderTitle';
+import { Image, TextInput, StyleSheet, View, Button } from 'react-native'
+import Header from '../components/Header'
+import BackIcon from '../components/icons/BackIcon'
+import HeaderTitle from '../components/HeaderTitle'
 const ForgotPassword = () => {
-
-return (
+  return (
+    // Todo: redo the whole page
     <View style={styles.container}>
-        <Header
-            leftComponent={<BackIcon />}
-            middleComponent={<HeaderTitle title="Restore Password" />}
+      <Header
+        leftComponent={<BackIcon />}
+        middleComponent={<HeaderTitle title="Restore Password" />}
+      />
+      <View style={{ padding: '10%' }}>
+        <Image
+          style={{ alignSelf: 'center', height: '45%', width: '60%' }}
+          source={require('../images/cry.png')}
         />
-        <View style={{padding:'10%',}}>
-            <Image style={{alignSelf:'center',height:'45%',width:'60%'}} source={require('./src/ui/images/cry.png')}/>
-            <TextInput style={styles.text} placeholder="Enter email"/>
-            <Button
-                title="Send E-mail"
-                color="#5956E9"
-            />
-        </View>
+        <TextInput style={styles.text} placeholder="Enter email" />
+        <Button title="Send E-mail" color="#5956E9" />
+      </View>
     </View>
-);
-};
+  )
+}
 
 const styles = StyleSheet.create({
-container: {
+  container: {
     flex: 1,
     backgroundColor: '#E5E5E5',
-},
-text:{
-    backgroundColor:'#ffffff',
-    borderRadius:5,
-    alignItems:'center',
-    marginVertical:'4%',
-    borderWidth: 1,    
-}
-});
+  },
+  text: {
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    alignItems: 'center',
+    marginVertical: '4%',
+    borderWidth: 1,
+  },
+})
 export default ForgotPassword
