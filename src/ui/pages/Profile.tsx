600d4ee86a5760d47fe26e96db0248ee0814cd8b
mostafaelspagh0 <mostafaelspagh0@gmail.com>
2022-05-02 05:34:03 +0200

    rework profile page

--- src/ui/pages/Profile.tsx
+++ src/ui/pages/Profile.tsx
@@ -1,99 +1,116 @@
 import * as React from 'react'
-import { memo, useState } from 'react';
-import { TouchableOpacity,Image,StyleSheet, Text, View } from 'react-native'
-const Profile = () => {
-
-
-return (
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../redux/user/userSlice'
+import Header from '../components/Header'
+import HeaderBackButton from '../components/HeaderBackButton'
+import LocationIcon from '../components/icons/LocationIcon'
+import ProfileButton from '../components/ProfileButton'
+const Profile = ({ navigation }: any) => {
+  const user = useSelector(selectUser)
+  return (
     <View style={styles.container}>
-      <TouchableOpacity>
-        <Image style={{
-            height:20 ,
-            width:20,
-        }} source={require('../images/back.png')} />
-      </TouchableOpacity>
-      <Text style={{
-        fontFamily: 'Raleway-SemiBold',
-        fontSize: 34,
-        fontWeight: '800',
-        color:'#000000',
-        paddingBottom:30,
-      }}>My profile</Text>
-      <View style={styles.button}>
-        <Image style={{
-            height:76 ,
-            width:76,
-            alignSelf:'center',
-            borderRadius:76,
-            // resizeMode: 'contain',
-            paddingVertical:20,
-            marginTop:-50
-        }} source={require('../images/photo.png')} />
-          <Text style={{
-            alignSelf:'center',
-            fontFamily:'Raleway-SemiBold',
-            fontSize:18,fontWeight: '600',
-            color:'#000000',
-          }}>Name</Text>
-          <View style={{flexDirection:'row',}}>
-            <Image style={{
-              height:20 ,
-              width:20,
-            }} source={require('../images/location.png')} />
-            <Text style={{
-              fontFamily:'Raleway-SemiBold',
-              fontSize:15,fontWeight: '400',
-              color:'#000000',
-            }}>Address: 43 cairo</Text>
+      <Header leftComponent={<HeaderBackButton navigation={navigation} />} />
+      <View style={{ flex: 1, marginHorizontal: 20 }}>
+        <Text
+          style={{
+            fontFamily: 'Raleway-SemiBold',
+            fontSize: 34,
+            fontWeight: '800',
+            color: '#000000',
+            paddingBottom: 30,
+          }}
+        >
+          My profile
+        </Text>
+        <View style={[styles.button, { marginTop: 40 }]}>
+          <Image
+            style={{
+              height: 100,
+              width: 100,
+              alignSelf: 'center',
+              borderRadius: 76,
+              resizeMode: 'contain',
+              paddingVertical: 20,
+              marginTop: -70,
+            }}
+            source={{ uri: user.image }}
+          />
+          <Text
+            style={{
+              alignSelf: 'center',
+              fontFamily: 'Raleway-SemiBold',
+              fontSize: 18,
+              fontWeight: '600',
+              color: '#000000',
+              marginTop: 10,
+            }}
+          >
+            {`${user.firstName} ${user.lastName}`}
+          </Text>
+          <View style={{ flexDirection: 'row', marginTop: 10 }}>
+            <LocationIcon style={{ marginHorizontal: 20 }} />
+            <View>
+              <Text style={styles.addressText}>Address: 43 cairo</Text>
+              <Text style={styles.addressText}>M13 4GR</Text>
+              <Text style={styles.addressText}>Manchester, UK</Text>
+            </View>
           </View>
+        </View>
+        <ProfileButton
+          text={'Edit Profile'}
+          onPress={function (): void {
+            throw new Error('Function not implemented.')
+          }}
+        />
+        <ProfileButton
+          text="Shopping address"
+          onPress={function (): void {
+            throw new Error('Function not implemented.')
+          }}
+        />
+        <ProfileButton
+          text="Order History"
+          onPress={function (): void {
+            throw new Error('Function not implemented.')
+          }}
+        />
+        <ProfileButton
+          text="Cards"
+          onPress={function (): void {
+            throw new Error('Function not implemented.')
+          }}
+        />
+        <ProfileButton
+          text="Notifications"
+          onPress={function (): void {
+            throw new Error('Function not implemented.')
+          }}
+        />
       </View>
-
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>
-            Edit profile</Text>
-        </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>
-            Shopping address</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.text}>
-              Order history</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>
-            Cards</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>
-            Notifications</Text>
-        </TouchableOpacity>
-
     </View>
-  );
-};
-  
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#E5E5E5',
-    paddingHorizontal: '10%',
-    paddingBottom: '10%',
-    paddingVertical:'10%',
   },
-  text:{
+  text: {
     fontFamily: 'Raleway-SemiBold',
     fontSize: 18,
-    fontWeight: '600' ,
-    color:'#000000'
+    color: '#000000',
   },
-  button:{
-    backgroundColor:'#ffffff',
-    padding:15,
-    borderRadius:20,
-    marginVertical:3,
+  button: {
+    backgroundColor: '#ffffff',
+    padding: 15,
+    borderRadius: 20,
   },
-});
+  addressText: {
+    fontFamily: 'Raleway-Regular',
+    fontSize: 15,
+    color: '#000000',
+  },
+})
 export default Profile
