f90cf1b40198cbea017dee794d77e0bb459b9dfc
AbdelrahmanMohammedSayed <91883530+AbdelrahmanMohammedSayed@users.noreply.github.com>
2022-05-09 16:25:50 +0200

    update Register screen

--- src/ui/pages/Register.tsx
+++ src/ui/pages/Register.tsx
@@ -1,61 +1,125 @@
 import * as React from 'react'
-import { TouchableOpacity,Image,TextInput,StyleSheet, Text, View, Button } from 'react-native'
-import Header from '../components/Header';
-import BackIcon from '../components/icons/BackIcon';
-import HeaderTitle from '../components/HeaderTitle';
+import {
+  TouchableOpacity,
+  Image,
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native'
+import Header from '../components/Header'
+import BackIcon from '../components/icons/BackIcon'
+import HeaderTitle from '../components/HeaderTitle'
+import LargeBottomButton from '../components/LargeBottomButton'
+import LoginFormField from '../components/LoginFormField'
+import EmailIcon from '../components/icons/EmailIcon'
+import LockIcon from '../components/icons/LockIcon'
 const Register = () => {
-
-return (
+  return (
     <View style={styles.container}>
-    
-        <Header
-        leftComponent={<BackIcon />}
-        middleComponent={<HeaderTitle title="Create Account" />}
+
+      <View
+        style={[
+          {
+            flex: 1,
+            maxHeight: '30%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        ]}
+      >
+        <Image
+          source={require('../images/Welcom.png')}
+          resizeMode={'cover'}
+          style={{ position: 'absolute', top: 0, width: '100%' }}
+        />
+        <Text style={styles.header}>Welcome!</Text>
+      </View>
+
+      <View style={styles.box}>
+        <Text style={styles.boxTitle}>Create account</Text>
+        <LoginFormField
+          icon={<EmailIcon />}
+          label={'Email'}
+          placeHolder="ex: hello@world.com"
+          style={{
+            marginVertical: 0,
+          }}
         />
 
+      <LoginFormField
+        icon={<LockIcon />}
+        label={'Password'}
+        placeHolder="************"
+        style={{
+          marginVertical: 5,
+        }}
+      />
+      <LoginFormField
+        icon={<LockIcon />}
+        label={'Confirm Password'}
+        placeHolder="************"
+        style={{
+          marginVertical: 5,
+        }}
+      />
+      <LargeBottomButton
+            text={'Create account'}
+            color={'#5956E9'}
+            width={'100%'}
+            height={50}
+            textColor={'#FFFFFF'}
+          />
+        <TouchableOpacity style={{ paddingVertical: 5 }}>
+          <Text style={{ fontFamily: 'Raleway-SemiBold',
+                        fontSize: 18,fontWeight: 'bold',
+                        color: '#5956E9', alignSelf: 'center' }}>
+            Login
+          </Text>
+        </TouchableOpacity>
+      </View>
 
-        <View style={{paddingHorizontal:'10%'}}>
-            <Image style={{height:'30%' ,width:'45%',alignSelf:'center'}} source={require('./src/ui/images/Saly-11.png')}/>
-
-            <TextInput style={styles.text} placeholder="Name"/>
-            <TextInput style={styles.text} placeholder="Email"/>
-            <TextInput style={styles.text} placeholder="Password"/>
-            <Button
-            title="Sign up"
-            color="#5956E9"
-            />
-
-            <View style={{flexDirection:'row',alignSelf:'center'}}>
-                <Text style={{
-                alignSelf: 'center',color:'#000000',
-                fontFamily:'Raleway-SemiBold',
-                fontSize:18,fontWeight:'600'
-                }}>Already have an account?</Text>
-                <TouchableOpacity>
-                <Text style={{
-                color:'#5956E9',padding:5,
-                fontFamily:'Raleway-SemiBold',
-                fontSize:18,fontWeight:'bold',
-                }}>Login</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
     </View>
-);
-};
+  )
+}
 
 const styles = StyleSheet.create({
-container: {
+  container: {
     flex: 1,
-    backgroundColor: '#E5E5E5',
-},
-text:{
-    backgroundColor:'#ffffff',
-    borderRadius:5,
-    alignItems:'center',
-    marginVertical:'4%',
-    borderWidth: 1,    
-    }
-});
+    backgroundColor: '#5956E9',
+  },
+  header: {
+    fontSize: 65,
+    fontFamily: 'Raleway-ExtraBold',
+    color: '#FFFFFF',
+  },
+  design: {
+    flex: 1,
+    height: '30%',
+  },
+  box: {
+    flex: 1,
+    paddingTop: '0%',
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20,
+    paddingHorizontal: 50,
+    backgroundColor: '#ffffff',
+  },
+  boxTitle: {
+    fontFamily: 'Raleway-SemiBold',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginVertical: 10,
+  },
+  text: {
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    alignItems: 'center',
+    marginVertical: '4%',
+    borderWidth: 1,
+  },
+})
 
 export default Register
