64dceedbdce5f66a396764ce0612cf49a5f8094e
AbdelrahmanMohammedSayed <91883530+AbdelrahmanMohammedSayed@users.noreply.github.com>
2022-05-09 22:41:11 +0200

    edit path images in Basket

--- src/ui/pages/Basket.tsx
+++ src/ui/pages/Basket.tsx
@@ -1,87 +1,116 @@
 import * as React from 'react'
-import { TouchableOpacity,ScrollView,Image,TextInput,StyleSheet, Text, View, Button } from 'react-native'
-import Header from '../components/Header';
-import BackIcon from '../components/icons/BackIcon';
-import HeaderTitle from '../components/HeaderTitle';
-import LargeBottomButton from '../components/LargeBottomButton';
+import {
+  TouchableOpacity,
+  ScrollView,
+  Image,
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native'
+import Header from '../components/Header'
+import BackIcon from '../components/icons/BackIcon'
+import HeaderTitle from '../components/HeaderTitle'
+// import LargeBottomButton from '../components/LargeBottomButton'
 const Basket = () => {
-
-return (
+  return (
     <View style={styles.container}>
-        <Header
+      <Header
         leftComponent={<BackIcon />}
         middleComponent={<HeaderTitle title="Basket" />}
-        rightComponent={<TouchableOpacity><Image  source={require('./src/ui/images/Delete.png')}/></TouchableOpacity>}
-        />
+        rightComponent={
+          <TouchableOpacity>
+            <Image source={require('../images/Delete.png')} />
+          </TouchableOpacity>
+        }
+      />
 
-        <View style={{height:'60%',}} >
-        <View style={{
-        flexDirection:'row',
-        backgroundColor:'#ffffff',
-        alignSelf:'center',
-        width:'75%',
-        height:'35%',
-        borderRadius:10,
-        padding:'2%',
-        }}>
-            <Image  source={require('./src/ui/images/Tab.png')}/>
-            <View style={{paddingVertical:'7%',paddingHorizontal:'5%'}}>
-            <Text>
-                Name
-            </Text>
-            <Text>
-                0000$
-            </Text>
-            <View style={{flexDirection:'row',}}>
-                <Text>
-                Quantity
+      <View style={{ height: '60%' }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: '#ffffff',
+            alignSelf: 'center',
+            width: '75%',
+            height: '35%',
+            borderRadius: 10,
+            padding: '2%',
+          }}
+        >
+          <Image source={require('../images/Tab.png')} />
+          <View style={{ paddingVertical: '7%', paddingHorizontal: '5%' }}>
+            <Text>Name</Text>
+            <Text>0000$</Text>
+            <View style={{ flexDirection: 'row' }}>
+              <Text>Quantity</Text>
+              <TouchableOpacity style={{ paddingHorizontal: 5 }}>
+                <Text
+                  style={{
+                    backgroundColor: '#7DCCEC',
+                    paddingHorizontal: '4%',
+                    borderRadius: 5,
+                  }}
+                >
+                  -
                 </Text>
-                <TouchableOpacity style={{paddingHorizontal:5}} >
-                <Text style={{backgroundColor:'#7DCCEC',paddingHorizontal:'4%',borderRadius:5}}>
-                    -
+              </TouchableOpacity>
+              <Text>1</Text>
+              <TouchableOpacity style={{ paddingHorizontal: 5 }}>
+                <Text
+                  style={{
+                    backgroundColor: '#7DCCEC',
+                    paddingHorizontal: '4%',
+                    borderRadius: 5,
+                  }}
+                >
+                  +
                 </Text>
-                </TouchableOpacity>
-                <Text>
-                1
-                </Text>
-                <TouchableOpacity style={{paddingHorizontal:5}} >
-                <Text style={{backgroundColor:'#7DCCEC',paddingHorizontal:'4%',borderRadius:5}}>
-                    +
-                </Text>
-                </TouchableOpacity>
+              </TouchableOpacity>
             </View>
-            </View>
+          </View>
         </View>
-        </View>
+      </View>
 
-        <View style={{flexDirection:'row',paddingVertical:'5%'}}>
-        <Text style={{position: 'absolute', left: '10%'}}>Total</Text>
-        <Text style={{position: 'absolute', right: '10%'}}>0000$</Text>
-        </View>
+      <View style={{ flexDirection: 'row', paddingVertical: '5%' }}>
+        <Text style={{ position: 'absolute', left: '10%' }}>Total</Text>
+        <Text style={{ position: 'absolute', right: '10%' }}>0000$</Text>
+      </View>
 
-        <View style = {{minHeight:'20%'}}>
-
-        <TouchableOpacity  style={{width:'80%',height:'30%',alignSelf:'center',alignItems:'center',backgroundColor:'#5956E9',borderRadius:20,}} >
-            <Text style={{
-            color:'#ffffff',paddingVertical:10,
-            fontFamily:'Raleway-SemiBold',
-            fontSize:18,fontWeight:'bold',}}>
+      <View style={{ minHeight: '20%' }}>
+        <TouchableOpacity
+          style={{
+            width: '80%',
+            height: '30%',
+            alignSelf: 'center',
+            alignItems: 'center',
+            backgroundColor: '#5956E9',
+            borderRadius: 20,
+          }}
+        >
+          <Text
+            style={{
+              color: '#ffffff',
+              paddingVertical: 10,
+              fontFamily: 'Raleway-SemiBold',
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}
+          >
             Checkout
-            </Text>
+          </Text>
         </TouchableOpacity>
-        </View>
-    </View>   
-);
-};
+      </View>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-container: {
+  container: {
     flex: 1,
     backgroundColor: '#E5E5E5',
-},
-item:{
-
-},
-});
+  },
+  item: {},
+})
 
 export default Basket
