0351d6537e4a3b76676a966412e4183be8bbd749
mostafaelspagh0 <mostafaelspagh0@gmail.com>
2022-05-09 12:41:01 +0200

    implement home page - ( item card )

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -12,94 +12,157 @@
 // import { increment, selectCounter } from '../../redux/counter/counterSlice'
 
 import React from 'react'
-import { Text, View } from 'react-native'
-import DrawerOpenButton from '../components/DrawerOpenButton'
+import { Text, useWindowDimensions, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view'
 import Header from '../components/Header'
+import HeaderBackButton from '../components/HeaderBackButton'
+import BagIcon from '../components/icons/BagIcon'
 import SearchBar from '../components/SearchBar'
 
-// const Home = () => {
-//   const count = useSelector(selectCounter)
-//   const dispatch = useAppDispatch()
-//   const isPressed = useSharedValue(false)
-//   const offset = useSharedValue({ x: 0, y: 0 })
-//   const animatedStyles = useAnimatedStyle(() => {
-//     return {
-//       transform: [
-//         { translateX: offset.value.x },
-//         { translateY: offset.value.y },
-//         { scale: withSpring(isPressed.value ? 1.2 : 1) },
-//       ],
-//       backgroundColor: isPressed.value ? 'yellow' : 'blue',
-//     }
-//   })
-//   const increase = () => {
-//     dispatch(increment())
-//   }
-//   const start = useSharedValue({ x: 0, y: 0 })
-//   const gesture = Gesture.Pan()
-//     .onBegin(() => {
-//       runOnJS(increase)()
-//       isPressed.value = true
-//     })
-//     .onUpdate((e) => {
-//       offset.value = {
-//         x: e.translationX + start.value.x,
-//         y: e.translationY + start.value.y,
-//       }
-//     })
-//     .onEnd(() => {
-//       start.value = {
-//         x: offset.value.x,
-//         y: offset.value.y,
-//       }
-//     })
-//     .onFinalize(() => {
-//       isPressed.value = false
-//     })
+const FirstRoute = () => {
+  return <View style={{ flex: 1, backgroundColor: 'red' }} />
+}
+
+const SecondRoute = () => <View style={{ flex: 1, backgroundColor: 'blue' }} />
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+  third: FirstRoute,
+  fourth: SecondRoute,
+  fifth: FirstRoute,
+  sixth: SecondRoute,
+  seventh: FirstRoute,
+})
+
+export default function Home() {
+  const layout = useWindowDimensions()
+
+  const [index, setIndex] = React.useState(0)
+  const [routes] = React.useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+    { key: 'third', title: 'third' },
+    { key: 'fourth', title: 'fourth' },
+    { key: 'fifth', title: 'fifth' },
+    { key: 'sixth', title: 'sixth' },
+    { key: 'seventh', title: 'seventh' },
+  ])
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Header
+        leftComponent={<HeaderBackButton />}
+        middleComponent={
+          <SearchBar
+            text={''}
+            onChangeText={function (text: string): void {
+              throw new Error('Function not implemented.')
+            }}
+          />
+        }
+        rightComponent={<BagIcon color={'#200E32'} />}
+      />
+      <View
+        style={{
+          flex: 1,
+          maxHeight: 150,
+          justifyContent: 'center',
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 35,
+            fontFamily: 'Raleway-Bold',
+            marginHorizontal: 20,
+            marginEnd: 40,
+            color: '#000',
+          }}
+        >
+          Order online collect in store
+        </Text>
+      </View>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        swipeEnabled={false}
+        renderTabBar={(props) => (
+          <View style={{ marginHorizontal: 5 }}>
+            <TabBar
+              scrollEnabled={true}
+              {...props}
+              style={{
+                backgroundColor: 'transparent',
+              }}
+              labelStyle={{
+                color: '#000',
+                backgroundColor: 'transparent',
+              }}
+              tabStyle={{
+                backgroundColor: 'transparent',
+                width: layout.width / 5,
+              }}
+              contentContainerStyle={{
+                backgroundColor: 'transparent',
+              }}
+              indicatorStyle={{
+                backgroundColor: '#5956E9',
+              }}
+              renderTabBarItem={({ route: _route }) => {
+                let focus = index === routes.indexOf(_route)
+                return (
+                  <View
+                    key={_route.key}
+                    style={{
+                      marginHorizontal: 5,
+                      width: layout.width / 5 - 10,
+                      marginVertical: 5,
+                    }}
+                  >
+                    <TouchableOpacity
+                      onPress={() => {
+                        setIndex(routes.indexOf(_route))
+                      }}
+                    >
+                      <Text
+                        style={{
+                          alignSelf: 'center',
+                          color: focus ? '#5956E9' : '#9A9A9D',
+                          fontFamily: 'Raleway-SemiBold',
+                          fontSize: 17,
+                        }}
+                      >
+                        {_route.title}
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                )
+              }}
+              layout={layout}
+            />
+          </View>
+        )}
+        initialLayout={{ width: layout.width }}
+      />
+    </View>
+  )
+}
+
+// const Home = ({ navigation }: any) => {
 //   return (
-//     <View style={StyleSheet.absoluteFill}>
-//       <GestureDetector gesture={gesture}>
-//         <Animated.View
-//           style={[
-//             {
-//               height: 100,
-//               width: 100,
-//               backgroundColor: 'blue',
-//               borderRadius: 50,
-//               justifyContent: 'center',
-//               alignItems: 'center',
-//             },
-//             animatedStyles,
-//           ]}
-//         >
-//           <Text
-//             style={{
-//               fontSize: 40,
-//             }}
-//           >
-//             {count}
-//           </Text>
-//         </Animated.View>
-//       </GestureDetector>
+//     <View style={{ flex: 1, backgroundColor: '#F2F2F2' }}>
+//       <Header
+//         leftComponent={<DrawerOpenButton navigation={navigation} />}
+//         middleComponent={<SearchBar text={''} onChangeText={() => {}} />}
+//         disableRightComponent
+//       />
+//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//         <Text>Home Screen</Text>
+//       </View>
 //     </View>
 //   )
 // }
 
 // export default Home
-
-const Home = ({ navigation }: any) => {
-  return (
-    <View style={{ flex: 1, backgroundColor: '#F2F2F2' }}>
-      <Header
-        leftComponent={<DrawerOpenButton navigation={navigation} />}
-        middleComponent={<SearchBar text={''} onChangeText={() => {}} />}
-        disableRightComponent
-      />
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-    </View>
-  )
-}
-
-export default Home
