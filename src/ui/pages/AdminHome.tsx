3c96aff413b8aa5398560a344c2468ef78e1a61d
meliodas227 <yousefalaa2279@gmail.com>
2022-05-17 01:14:02 +0200

    added navigation and needed buttons

--- src/ui/pages/AdminHome.tsx
+++ src/ui/pages/AdminHome.tsx
@@ -1,40 +1,54 @@
 import { StyleSheet, Text, useWindowDimensions, View } from 'react-native'
 import React from 'react'
 import LargeBottomButton from '../components/LargeBottomButton'
+import Header from '../components/Header'
+import HeaderTitle from '../components/HeaderTitle'
 
-export default function AdminHome() {
-    const window = useWindowDimensions()
+export default function AdminHome({ navigation }: any) {
+  const window = useWindowDimensions()
   return (
     <View>
-      <Text
-        style={{
-          color: 'red',
-          alignSelf: 'center',
-          margin: 20,
-          fontSize: 25,
-          fontWeight: '800',
-        }}
-      >
-        Admin
-      </Text>
-      <View style={styles.buttonContainer}>
-        <LargeBottomButton color='#5956E9' height={window.height*.1} width={window.width*.75} text='hi' textColor='white' onPress={()=>{}}/>
-      </View>
-      <View style={styles.buttonContainer}>
-        <LargeBottomButton color='#5956E9' height={window.height*.1} width={window.width*.75} text='hi' textColor='white' onPress={()=>{}}/>
-      </View>
-      <View style={styles.buttonContainer}>
-        <LargeBottomButton color='#5956E9' height={window.height*.1} width={window.width*.75} text='hi' textColor='white' onPress={()=>{}}/>
-      </View>
-      <View style={styles.buttonContainer}>
-        <LargeBottomButton color='#5956E9' height={window.height*.1} width={window.width*.75} text='hi' textColor='white' onPress={()=>{}}/>
-      </View>
+      <Header
+        middleComponent={<HeaderTitle title={'Admin'} />}
+      />
+        <View style={{ marginVertical: window.height * 0.2 }}>
+          <View style={styles.buttonContainer}>
+            <LargeBottomButton
+              color="#5956E9"
+              height={window.height * 0.1}
+              width={window.width * 0.75}
+              text="Manage Products"
+              textColor="white"
+              onPress={() => {navigation.navigate('ProductManagement')}}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <LargeBottomButton
+              color="#5956E9"
+              height={window.height * 0.1}
+              width={window.width * 0.75}
+              text="Manage Orders"
+              textColor="white"
+              onPress={() => {navigation.navigate('OrderManagement')}}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <LargeBottomButton
+              color="#5956E9"
+              height={window.height * 0.1}
+              width={window.width * 0.75}
+              text="Manage Users"
+              textColor="white"
+              onPress={() => {navigation.navigate('UserManagement')}}
+            />
+          </View>
+        </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    buttonContainer:{
-        margin:5
-    }
+  buttonContainer: {
+    margin: 5,
+  },
 })
