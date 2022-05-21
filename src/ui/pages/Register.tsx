feaccdb5ed4c6586369bdd42a1cc8944f395886d
meliodas227 <yousefalaa2279@gmail.com>
2022-05-21 15:17:41 +0200

    added signup

--- src/ui/pages/Register.tsx
+++ src/ui/pages/Register.tsx
@@ -5,6 +5,7 @@ import { Controller, useForm } from 'react-hook-form'
 import { TouchableOpacity, StyleSheet, Text, View } from 'react-native'
 import useLogin from '../../hooks/useLogin'
 import { MainStackParamList } from '../../navigation/MainStack'
+import auth from '../../services/auth'
 import Header from '../components/Header'
 import HeaderBackButton from '../components/HeaderBackButton'
 import EmailIcon from '../components/icons/EmailIcon'
@@ -25,12 +26,11 @@ const Register = ({
     error: loginError,
     ignoreError,
   } = useLogin()
-  const { handleSubmit, control } = useForm<FormValues>()
+  const { register, control } = useForm<FormValues>()
   useEffect(() => {
     ignoreError()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-
   const [show, setshow] = React.useState(true)
 
   return (
@@ -118,38 +118,6 @@ const Register = ({
           name={'password'}
         />
 
-        <Controller
-          control={control}
-          render={({ field: { onChange, value }, fieldState: { error } }) => {
-            return (
-              <LoginFormField
-                icon={<LockIcon />}
-                label={'Password'}
-                placeHolder="* * * * * * *"
-                style={{
-                  marginVertical: 10,
-                }}
-                isShowable
-                secureTextEntry={show}
-                value={value}
-                onChangeText={onChange}
-                error={error?.message}
-                show={() => setshow(!show)}
-              />
-            )
-          }}
-          rules={{
-            required: {
-              value: true,
-              message: 'Password is required',
-            },
-            minLength: {
-              value: 6,
-              message: 'Password must be at least 6 characters',
-            },
-          }}
-          name={'password'}
-        />
         {loginError ? <Text style={styles.error}>{loginError}</Text> : null}
 
         <TouchableOpacity style={{ paddingVertical: 10 }}>
@@ -158,27 +126,18 @@ const Register = ({
 
         <View style={{ marginVertical: 5 }}>
           <LargeBottomButton
-            text={'Login'}
+            text={'Sign up'}
             color={'#5956E9'}
             width={'100%'}
             height={70}
             textColor={'#FFFFFF'}
-            onPress={handleSubmit(({ email, password }) =>
-              loginWithEmailAndPassword(email, password)
-            )}
+            onPress={() => {
+              auth.signUp
+              navigation.navigate('Login')
+            }}
             loading={loading}
           />
         </View>
-        <TouchableOpacity
-          style={{ paddingVertical: 10 }}
-          onPress={() => {
-            navigation.navigate('Regiter')
-          }}
-        >
-          <Text style={{ color: '#5956E9', alignSelf: 'center' }}>
-            Create Account
-          </Text>
-        </TouchableOpacity>
       </View>
     </View>
   )
